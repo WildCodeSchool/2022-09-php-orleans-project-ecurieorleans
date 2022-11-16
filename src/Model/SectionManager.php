b29951cad74124185803a50981d25109fdbce744
V-Gillet <gillet.valentin45000@gmail.com>
2022-11-16 16:44:14 +0100

    finished add crud

--- src/Model/SectionManager.php
+++ src/Model/SectionManager.php
@@ -7,4 +7,15 @@ use PDO;
 class SectionManager extends AbstractManager
 {
     public const TABLE = 'section';
+
+    public function insert(array $section, ?string $uniqueFileName): int
+    {
+        $statement = $this->pdo->prepare("INSERT INTO " . self::TABLE . " (`name`, header, presentation) VALUES (:name, :header, :presentation)");
+        $statement->bindValue('name', $section['name'], PDO::PARAM_STR);
+        $statement->bindValue('header', $uniqueFileName, PDO::PARAM_STR);
+        $statement->bindValue('presentation', $section['presentation'], PDO::PARAM_STR);
+
+        $statement->execute();
+        return (int)$this->pdo->lastInsertId();
+    }
 }

9b291e4ac61eb7f6cd625469a0f6a7ec5a169912
NefytZ <thyphen.merigot@gmail.com>
2022-11-21 23:13:22 +0100

    2nd version of navbar

--- src/routes.php
+++ src/routes.php
@@ -7,7 +7,7 @@
 //          3. (optional) array of query string keys to send as parameter to the method
 // e.g route '/item/edit?id=1' will execute $itemController->edit(1)
 return [
-    '' => ['HomeController', 'index',],
+    'home' => ['HomeController', 'index',],
     'admin/evenement' => ['EventAdminController', 'index'],
     'admin/evenement/add' => ['EventAdminController', 'add'],
     'admin/evenement/edit' => ['EventAdminController', 'edit', ['id']],

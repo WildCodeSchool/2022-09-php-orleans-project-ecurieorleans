51af6975c67a5aeaf2c12adb635afd2ec2e676bd
HuHoudas <hugo.houdas@epitech.eu>
2022-11-22 10:47:16 +0100

    mentions legales

--- src/Controller/MentionLegaleController.php
+++ src/Controller/MentionLegaleController.php
@@ -2,8 +2,22 @@
 
 namespace App\Controller;
 
-class MentionLegaleController extends AbstractController
+use Twig\Environment;
+use Twig\Loader\FilesystemLoader;
+class MentionLegaleController
 {
+    protected Environment $twig;
+    public function __construct()
+    {
+        $loader = new FilesystemLoader(APP_VIEW_PATH);
+        $this->twig = new Environment(
+            $loader,
+            [
+                'cache' => false,
+                'debug' => true,
+            ]
+        );
+    }
     public function index()
     {
         return $this->twig->render("MentionsLegale/MentionsLegale.html.twig");

ebdbf84a863cf0af50e477f2153240fe6862f0e4
HuHoudas <hugo.houdas@epitech.eu>
2022-11-23 11:14:57 +0100

    fix delete member

--- src/Controller/AdminBoardController.php
+++ src/Controller/AdminBoardController.php
@@ -6,18 +6,17 @@ use App\Model\BoardManager;
 
 class AdminBoardController extends AbstractController
 {
-    private const INPUT_MAX_LENGTH = 25;
+    private const INPUT_MAX_LENGHT = 25;
     private const ROLES = [
         "Président d'honneur", "Président", "Vice-président", "Secrétaire", "Secrétaire adjoint",
         "Trésorier", "Trésorier adjoint", "Entraineur"
     ];
-    public const RESPONSABILITIES = ["Responsable", "Adjoint", "Entraîneur"];
 
     public function add()
     {
         $membersErrors = [];
         $membersManager = new BoardManager();
-        $responsabilities = self::RESPONSABILITIES;
+        $responsability = ["Responsable", "Adjoint", "Entraîneur"];
         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             $members = array_map('trim', $_POST);
             if (isset($members["boardmember"])) {
@@ -36,7 +35,7 @@ class AdminBoardController extends AbstractController
         return $this->twig->render('AdminBoard/AdminAddBoard.html.twig', [
             'errors' => $membersErrors,
             'roles' => self::ROLES,
-            'sectionResponsabilities' => $responsabilities,
+            'sectionResponsabilitys' => $responsability,
         ]);
     }
 
@@ -58,7 +57,7 @@ class AdminBoardController extends AbstractController
             "Trésorier", "Trésorier adjoint", "Entraineur"
         ];
         $member = $membersManager->selectOneById($id);
-        $responsabilities = self::RESPONSABILITIES;
+        $responsability = ["Responsable", "Adjoint", "Entraîneur"];
         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             $members = array_map('trim', $_POST);
             if (isset($members["boardmember"])) {
@@ -77,7 +76,7 @@ class AdminBoardController extends AbstractController
         return $this->twig->render('AdminBoard/AdminEditBoard.html.twig', [
             'errors' => $membersErrors,
             'roles' => $roles,
-            'sectionResponsabilities' => $responsabilities,
+            'sectionResponsabilitys' => $responsability,
             'member' => $member,
         ]);
     }
@@ -95,18 +94,21 @@ class AdminBoardController extends AbstractController
         }
 
         if (!filter_var($member['email'], FILTER_VALIDATE_EMAIL)) {
-            $errors[] = 'Votre adresse mail n\'est pas au bon format.';
+            $errors[] = 'le mail n\'a pas le bon format.';
         }
 
         if (in_array($member['role'], $roles)) {
-            $errors[] = 'Le rôle doit être un des rôles valide.';
+            $errors[] = 'Le rôle doit etre valide.';
+        }
+        if (strlen($member['phone']) != 10 || !filter_var($member['phone'], FILTER_VALIDATE_INT)) {
+            $errors[] = "le numéro de téléphone n\'a pas le bon format.";
         }
 
-        if (strlen($member['lastname']) > self::INPUT_MAX_LENGTH) {
-            $errors[] = 'Le nom du membre doit faire moins de ' . self::INPUT_MAX_LENGTH . ' caractères.';
+        if (strlen($member['lastname']) > self::INPUT_MAX_LENGHT) {
+            $errors[] = 'Le nom du membre doit faire moins de ' . self::INPUT_MAX_LENGHT . ' caractères.';
         }
-        if (strlen($member['firstname']) > self::INPUT_MAX_LENGTH) {
-            $errors[] = 'Le prénom du membre doit faire moins de ' . self::INPUT_MAX_LENGTH . ' caractères.';
+        if (strlen($member['firstname']) > self::INPUT_MAX_LENGHT) {
+            $errors[] = 'Le nom du membre doit faire moins de ' . self::INPUT_MAX_LENGHT . ' caractères.';
         }
         return $errors;
     }
